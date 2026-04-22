c769d8d9bec5d07f86c4ced52123cca2d9b42d2f
jesus barboza <jbarz20@gmail.com>
2026-04-22 15:31:30 -0500

    FIX: remove deprecated xapikey

--- lib/features/buyer/restaurant_detail/restaurant_detail_service.dart
+++ lib/features/buyer/restaurant_detail/restaurant_detail_service.dart
@@ -1,8 +1,6 @@
+import 'package:komi_fe/core/config/app_config.dart';
 import 'package:komi_fe/core/network/http_client.dart';
 import 'package:komi_fe/features/buyer/restaurant_detail/restaurant_detail_model.dart';
-
-const String _kApiKey =
-    '45b9acda89d0119093fd77ff05b98b4696d982f964f2153ee2616a41af056fe0';
 
 class RestaurantDetailService {
   RestaurantDetailService(this._client);
@@ -13,7 +11,7 @@ class RestaurantDetailService {
   Future<StoreMenu> fetchMenu(String storeId) {
     return _client.get<StoreMenu>(
       '/v1/store/menu',
-      headers: {'x-api-key': _kApiKey},
+      headers: {'x-api-key': AppConfig.xApiKey},
       queryParams: {'store-id': storeId.trim()},
       fromJson: StoreMenu.fromJson,
     );
